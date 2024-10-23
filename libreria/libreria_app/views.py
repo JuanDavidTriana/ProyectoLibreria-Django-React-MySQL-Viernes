162791a084da443b210b7a7d7960511fa33de3c7
Juan David Triana Turmeque <88797895+JuanDavidTriana@users.noreply.github.com>
2024-10-23 16:22:58 -0500

    doc

--- libreria/libreria_app/views.py
+++ libreria/libreria_app/views.py
@@ -3,9 +3,16 @@ from .models import Libro, Autor
 from .serializers import AutorSerializer, LibroSerializer
 
 class AutorViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet que permite crear, leer, actualizar y eliminar Autores
+    """
     queryset = Autor.objects.all()
     serializer_class = AutorSerializer
 
+
 class LibroViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet que permite crear, leer, actualizar y eliminar Libros
+    """
     queryset = Libro.objects.all()
     serializer_class = LibroSerializer
